0581741c4b691ee8cd65b844f2b20f4f0506e1d2
Yuthala <dinapolousova@gmail.com>
2024-03-11 15:34:19 +0300

    fetch uncommented

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 
 const App = () => {
-  //const newLocal = 'http://89.111.141.36:8000/web-data';
+  const newLocal = 'http://89.111.141.36:8000/web-data';
   const handleClick = () => {
     console.log('button clicked');
-      //const data = {
-  //     products: 'apple',
-  //     totalPrice: '150',
-  // }
+      const data = {
+      products: 'apple',
+      totalPrice: '150',
+  }
 
-    // fetch(newLocal , {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify(data)
-    // })   
+    fetch(newLocal , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })   
   }
 
 
