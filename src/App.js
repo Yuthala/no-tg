7fb2d9de8ea46276f4d7e9c662fdde859e48cbc5
Yuthala <dinapolousova@gmail.com>
2024-03-11 15:19:35 +0300

    fetch commented

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
 //import logo from './logo.svg';
 import './App.css';
-import Button from './Button';
-import Product from './Product';
-
-// const App = () => {
-//     console.log('button clicked');
-
-//     const newLocal = 'http://89.111.141.36:8000/web-data';
-
-//   const handleClick = () => {
-
-//     const data = {
-//       products: 'apple',
-//       totalPrice: '150',
-//   }
-
-//     fetch(newLocal , {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(data)
-//     })   
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={handleClick}>Click</button>
-//     </div>
-//   );
-// };
-
-
-// export default App;
 
 
 const App = () => {
@@ -44,13 +11,13 @@ const App = () => {
       totalPrice: '150',
   }
 
-    fetch(newLocal , {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })   
+    // fetch(newLocal , {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(data)
+    // })   
   }
 
 
@@ -63,32 +30,4 @@ const App = () => {
 
 export default App;
 
-// function App() {
-//   const newLocal = 'https://89.111.141.36:8000/web-data';
 
-  // const handleClick = () => {
-
-  //   const data = {
-  //     products: 'apple',
-  //     totalPrice: '150',
-  // }
-
-  //   fetch(newLocal , {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(data)
-  //   })   
-  // }
-
-
-//   return (
-//     <div className="App">
-//       <Product />
-//       <Button onClick={handleClick}/>
-//     </div>
-//   );
-// }
-
-// export default App;
